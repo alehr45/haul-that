8a93bea9b87e59d9dcea219313a194ffaf5d56ab
Your NameAlex <alehr45@gmail.com>
2021-06-02 23:20:26 -0500

    css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,20 +7,21 @@ import image from "../images/homeimage.png";
 function Home() {
   return (
     <div>
-      <h1 class="home">HaulThat</h1>
+      <h1 class="home">Haul-That</h1>
       <h2 class="subtitle">A pickup/delivery service for larger items</h2>
     <div className="logo">
       <Image src={image}
         height="320px"
         width="380px"
+        
         roundedCircle
       />
       </div>
       <div class="logsign">
-        <Button className="font1" href="/login" variant="outline-danger">
+        <Button className="font1" href="/login" variant="success">
           Login
         </Button>{" "}
-        <Button className="font1" href="/signup" variant="outline-danger">
+        <Button className="font1" href="/signup" variant="success">
           Sign-Up
         </Button>{" "}
       </div>
