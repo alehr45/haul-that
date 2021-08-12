89450cdd355ba8db2fbdc3223f48e8a2e03b296a
zakonik <zakonik2417@gmail.com>
2021-08-12 11:16:09 -0400

    details page

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { Card, ListGroup, ListGroupItem, Button, Row, Col } from "react-bootstrap";
 
-const Details = () => {
-  var hello;
+const Details = (props) => {
+  console.log(props.name);
 
   return (
-    <Col>Hello</Col>
+    <Col>{props.name}</Col>
   );
 }
 
