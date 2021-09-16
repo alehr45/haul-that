f8d3c19e2c79c3287873ee9bd49642bafbf66be8
w.hunt <wfh2d88@gmail.com>
2021-09-16 14:42:04 -0400

    status

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -20,120 +20,32 @@ import DriverProfile from "./DriverProfile";
 import CustomerProfile from "./CustomerProfile";
 
 const Profile = () => {
-  // const [completeJob] = useMutation(COMPLETE_JOB);
   const { loading: userLoading, data } = useQuery(QUERY_ME_BASIC);
   const { loading: jobsLoading, data: jobsData } = useQuery(GET_JOBS);
 
   var user = {};
   var jobs = [];
-  // var completedJobs = [];
-  // var incompleteJobs = [];
 
   if (!userLoading) {
     user = data.me;
   }
-
-  // if(!jobsLoading){
-  //   jobs = jobsData.jobs
-  // }
-
-  // if(jobs){
-  //   for (let i = 0; i < jobs.length; i++) {
-  //     if (jobs[i].completed === false && jobs[i].driverUsername === user.username) {
-  //       incompleteJobs.push(jobs[i]);
-  //     }
-  //   }
-  //   for (let i = 0; i < jobs.length; i++) {
-  //     if (jobs[i].completed === true && jobs[i].driverUsername === user.username) {
-  //       completedJobs.push(jobs[i]);
-  //     }
-  //   }
-  // };
-
-  // const handleComplete = async (_id) => {
-  //   await completeJob({
-  //     variables: {
-  //       _id: _id,
-  //     },
-  //   });
-
-  //   // await emailjs.send("service_hsdqjea", "sign_up", formState, "user_VX87bNMDuxlz9E5XfnclG");
-
-  //   window.location.assign("/profile");
-  // };
 
   return (
     <Container className="profile2Form">
       <UserProfile user={user}></UserProfile>
 
       <Tabs
-        defaultActiveKey="home"
+        defaultActiveKey="customer"
         id="uncontrolled-tab-example"
         className="mb-3"
       >
-        <Tab eventKey="home" title="Customer">
+        <Tab eventKey="customer" title="Customer">
           <CustomerProfile jobs={jobs} />
         </Tab>
-        <Tab eventKey="profile" title="Driver">
+        <Tab eventKey="driver" title="Driver">
           <DriverProfile />
         </Tab>
       </Tabs>
-
-      {/* <Row>
-        <h1> Active Jobs</h1>
-        <div className="profilejob">
-          {incompleteJobs &&
-            incompleteJobs.map((job) => (
-              <Card
-                className="cardbody"
-                key={job._id}
-                style={{ width: "12rem" }}
-              >
-                <Card.Body>
-                  <Card.Title>Job # {job.id}</Card.Title>
-                </Card.Body>
-                <ListGroup className="list-group-flush"> */}
-      {/* <ListGroupItem>{job.date} </ListGroupItem> */}
-      {/* <ListGroupItem>
-                    Haul: {parseInt(job.distance)} miles{" "}
-                  </ListGroupItem>
-                  <ListGroupItem> {job.category} </ListGroupItem>
-                  <ListGroupItem>${parseInt(job.distance * 1.2)}</ListGroupItem>
-                  <Button
-                    variant="warning"
-                    onClick={() => handleComplete(job._id)}
-                  >
-                    Complete Job
-                  </Button>{" "}
-                </ListGroup>
-              </Card>
-            ))}
-        </div>
-        <h1 className="completed">Completed Jobs</h1>
-        <div className="profilejob">
-          {completedJobs &&
-            completedJobs.map((job) => (
-              <Card
-                className="cardbody"
-                key={job._id}
-                style={{ width: "12rem" }}
-              >
-                <Card.Body>
-                  <Card.Title>Job # {job.id}</Card.Title>
-                </Card.Body>
-                <ListGroup className="list-group-flush"> */}
-      {/* <ListGroupItem>{job.date} </ListGroupItem> */}
-      {/* <ListGroupItem>
-                    Haul: {parseInt(job.distance)} miles{" "}
-                  </ListGroupItem>
-                  <ListGroupItem> {job.category} </ListGroupItem>
-                  <ListGroupItem>${parseInt(job.distance * 1.2)}</ListGroupItem>
-                </ListGroup>
-              </Card>
-            ))}
-        </div>
-      </Row> */}
-      {/* <AcceptedJobs /> */}
     </Container>
   );
 };
