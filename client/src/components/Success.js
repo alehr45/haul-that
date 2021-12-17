10b7aa456fecbab1be3c0e3e78ab9d3683ba9845
zakonik <zakonik2417@gmail.com>
2021-12-17 12:52:07 -0500

    added completeJob to Success page

--- client/src/components/Success.js
+++ client/src/components/Success.js
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { COMPLETE_JOB } from "../utils/mutation";
 import { Button } from "react-bootstrap";
 
 const Success = () => {
   const [seconds, setSeconds] = useState(10);
   var timeRemaining = seconds;
+
+  const [completeJob] = useMutation(COMPLETE_JOB);
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const job_Id = urlParams.get("id");
+
+  completeJob({
+    variables: { _id: job_Id },
+  });
 
   const countDown = () => {
     timeRemaining--;
