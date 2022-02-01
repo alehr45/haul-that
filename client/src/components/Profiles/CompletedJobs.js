589d20c8fe473d5dc49ac7f610c9b07ecd8ac8a6
Your NameAlex <alehr45@gmail.com>
2022-02-01 13:26:19 -0600

    deploy

--- client/src/components/Profiles/CompletedJobs.js
+++ client/src/components/Profiles/CompletedJobs.js
@@ -2,9 +2,13 @@ import React from "react"
 import { Card, ListGroup, ListGroupItem, Col } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import moment from "moment"
+import AliceCarousel from "react-alice-carousel"
+import "react-alice-carousel/lib/alice-carousel.css"
 
 const CompletedJobs = ({ info, setDriverEarning }) => {
   let { completedJobs } = info
+
+  const handleDragStart = e => e.preventDefault()
 
   function add(accumulator, a) {
     return accumulator + a
@@ -14,39 +18,42 @@ const CompletedJobs = ({ info, setDriverEarning }) => {
   const sum = totalEarnings.reduce(add, 0)
   const finalEarnings = sum / 100
 
+  const responsive = {
+    1024: { items: 3 },
+    0: { items: 1 }
+  }
+
   if (finalEarnings) {
     setDriverEarning(finalEarnings)
   }
 
-  const slicedJobs = completedJobs.slice(0, 3)
+  const completedJobsArr = () => {
+    const items = completedJobs.map(job => (
+      <div onDragStart={handleDragStart} role="presentation" className="profilejob">
+        <Card className="cardbody" key={job._id} style={{ width: "12rem" }}>
+          <ListGroupItem className="job-box">
+            <Col className="pt-2">
+              <h4>Job # {job.id}</h4>
+              <h6>{parseInt(job.distance)} miles</h6>
+              <h6>{moment(job.date).format("MMMM Do YYYY")}</h6>
+              <h6>{job.category}</h6>
+              <h6>
+                {"$"}
+                {(job.price / 100).toFixed(2)}
+              </h6>
+            </Col>
+          </ListGroupItem>
+        </Card>{" "}
+        <li> </li>
+      </div>
+    ))
+    console.log(items)
+    return <AliceCarousel mouseTracking items={items} responsive={responsive} disableButtonsControls={true} />
+  }
 
-  return (
-    <div className="profilejob">
-      {slicedJobs &&
-        slicedJobs.map(job => (
-          <Card className="cardbody" key={job._id} style={{ width: "12rem" }}>
-            <ListGroupItem className="job-box">
-              <Col className="pt-2">
-                <h4>Job # {job.id}</h4>
-                <h6>{parseInt(job.distance)} miles</h6>
-                <h6>{moment(job.date).format("MMMM Do YYYY")}</h6>
-                <h6>{job.category}</h6>
-                <h6>
-                  {"$"}
-                  {(job.price / 100).toFixed(2)}
-                </h6>
-              </Col>
-            </ListGroupItem>
-          </Card>
+  // const slicedJobs = completedJobs.slice(0, 3)
 
-          // <li>
-          //   <Link className="link4" to={`/job/${job._id}`}>
-          //     {moment(job.date).format("MMMM Do YYYY")}
-          //   </Link>
-          // </li>
-        ))}
-    </div>
-  )
+  return <div>{completedJobsArr()}</div>
 }
 
 export default CompletedJobs
