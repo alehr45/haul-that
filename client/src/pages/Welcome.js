df11523211fd27874174215f25511387aa449ba0
Your NameAlex <alehr45@gmail.com>
2022-02-13 12:43:47 -0600

    modal and ternary for navbar

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -3,6 +3,7 @@ import { Button, Col } from "react-bootstrap"
 import { useQuery, useMutation } from "@apollo/react-hooks"
 import { QUERY_ME_BASIC } from "../utils/queries"
 import { UPDATE_POSITION } from "../utils/mutation"
+import { useParams } from "react-router-dom"
 
 const Welcome = () => {
   const { data } = useQuery(QUERY_ME_BASIC)
@@ -36,14 +37,18 @@ const Welcome = () => {
 
   return (
     <div className="welcome">
-      <h1>Hello {newFirstName}</h1>
-      <h1>Welcome to Haul-That</h1>
-      <Col>
-        <h5>Preferred Use</h5>
-        <Button value="customer" onClick={handleSubmit}>
+      <h3>
+        Hello <strong>{newFirstName}</strong>,
+      </h3>
+      <h1 className="welcome-title ">Welcome to Haul-That</h1>
+      <Col className="p-3">
+        <h6>
+          <small>Choose a position to get started.</small>
+        </h6>
+        <Button variant="success" className="m-2" value="customer" onClick={handleSubmit}>
           Customer
         </Button>
-        <Button value="driver" onClick={handleSubmit}>
+        <Button className="m-2" value="driver" onClick={handleSubmit}>
           Driver
         </Button>
       </Col>
